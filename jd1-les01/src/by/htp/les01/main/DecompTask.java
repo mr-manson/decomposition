461b07b6130d594a204f6789dd88292b4055f31d
mr-manson <60350677+mr-manson@users.noreply.github.com>
2020-02-14 18:32:41 +0300

    Add files via upload

--- jd1-les01/src/by/htp/les01/main/DecompTask.java
+++ jd1-les01/src/by/htp/les01/main/DecompTask.java
@@ -3,47 +3,96 @@ package by.htp.les01.main;
 public class DecompTask {
 
 	public static void task01() {
-		int area = areaTri(-2, -1, 2, 3, 4, -2);
-		
+		int area = areaTriByPoint(-2, -1, 2, 3, 4, -2);
+
 		System.out.println("Площадь треугольника равна " + area);
 	}
-	
+
 	public static void task02() {
 		int x = 35;
 		int y = 28;
-		
-		int a = max (x, y);
-		int b = min (x, y);
-		int c = nod (a, b);
-		
+
+		int a = max(x, y);
+		int b = min(x, y);
+		int c = nod(a, b);
+
 		int nok = (a * b) / c;
-		
+
 		System.out.println("Наибольший общий делитель чисел " + a + " и " + b + " равен " + c);
 		System.out.println("Наименьшее общее кратное чисел " + a + " и " + b + " равно " + nok);
 	}
-	
+
 	public static void task03() {
-		int a = 15, b = 8, c = 31;
-		int d = max(max(a, b), c);
-		System.out.println(d);
+		int a = 12, b = 8, c = 32, d = 16;
+		int nod = nod(nod(a, b), nod(c, d));
+		System.out.println(nod);
 	}
-	
+
+	public static void task04() {
+		int a = 6, b = 8, c = 12;
+		int nod;
+		int nok;
+
+		nod = nod(nod(a, b), c);
+		nok = (a * b * c) / nod;
+
+		System.out.println(nok);
+	}
+
+	public static void task05() {
+		int a = 6, b = 8, c = 12;
+		int min;
+		int max;
+		int sum;
+
+		min = min(min(a, b), c);
+		max = max(max(a, b), c);
+		sum = sum(min, max);
+
+	}
+
+	public static void task06() {
+		int a = 6;
+		int areaHexagon;
+		areaHexagon = 6 * areaTriEquil(a);
+		System.out.println(areaHexagon);
+	}
+
+	public static void task07() {
+	}
+
+	public static void task09() {
+		int a = 3, b = 7, c = 11;
+		int nod;
+		nod = nod(nod(a, b), c);
+		if (Math.abs(nod) == 1) {
+			System.out.println("Числа " + a + ", " + b + ", " + c + " являются взаимно простыми");
+		} else {
+			System.out.println("Числа " + a + ", " + b + ", " + c + " не являются взаимно простыми");
+		}
+	}
+
+
+
 	public static void task14() {
 		int a = 3345;
 		int b = 3456;
 		quantity(a, b);
-	
-		
+
 	}
+
 	//
-	// 01. Треугольник задан координатами своих вершин. Написать метод для вычисления его площади.
-	public static int areaTri(int x1, int y1, int x2, int y2, int x3, int y3) {
+	// 01. Треугольник задан координатами своих вершин. Написать метод для
+	// вычисления его площади.
+	public static int areaTriByPoint(int x1, int y1, int x2, int y2, int x3, int y3) {
 		int area;
 		area = Math.abs(((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))) / 2;
 		return area;
 	}
+
 	//
-	// 02. Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного 
+	// 02. Написать метод(методы) для нахождения наибольшего общего делителя и
+	// наименьшего общего кратного
 	// двух натуральных чисел:
 	public static int max(int a, int b) {
 		int max = 0;
@@ -54,6 +103,7 @@ public class DecompTask {
 		}
 		return max;
 	}
+
 	public static int min(int a, int b) {
 		int min = 0;
 		if (a < b) {
@@ -63,6 +113,7 @@ public class DecompTask {
 		}
 		return min;
 	}
+
 	public static int nod(int a, int b) {
 		int x = 1;
 		while (a != 0 && b != 0) {
@@ -72,29 +123,36 @@ public class DecompTask {
 		}
 		return a;
 	}
+
 	//
-	// 03. 3. Написать метод(методы) для нахождения наибольшего общего делителя четырех натуральных чисел.
+	// 05. Написать метод(методы) для нахождения суммы большего и меньшего из трех
+	// чисел.
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
 	//
 	//
 	//
 	//
 	//
 	//
-	// 14. Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр.
+	// 14. Написать метод(методы), определяющий, в каком из данных двух чисел больше
+	// цифр.
 	public static void quantity(int a, int b) {
 		int aa = a;
 		int bb = b;
-;		
+		;
 		int countA = 0;
 		int countB = 0;
-		
+
 		while (aa != 0) {
 			aa = aa / 10;
-			countA ++;
+			countA++;
 		}
 		while (bb != 0) {
 			bb = bb / 10;
-			countB ++;
+			countB++;
 		}
 		if (countA > countB) {
 			System.out.println("В числе " + a + " цифр больше, чем в числе " + b);
@@ -104,5 +162,11 @@ public class DecompTask {
 			System.out.println("В числах " + a + " и " + b + " количество цифр одинаковое");
 		}
 	}
-	
+
+	public static int areaTriEquil(int a) {
+		int area;
+		area = (int) (((3 * Math.sqrt(3)) / 2) * (a * a));
+		return area;
+	}
+
 }
